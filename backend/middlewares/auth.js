9d679002bb0f729ff64c189d4b92dec549831c2c
Ivan Shein <ivan-shein-yar@yandex.ru>
2023-04-15 00:40:29 +0300

    fix: исправить ошибку авторизации пользователя

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,5 +1,8 @@
+require('dotenv').config();
 const jwt = require('jsonwebtoken');
 const UnauthorizedError = require('../errors/unauthorized-err');
+
+const { NODE_ENV, JWT_SECRET } = process.env;
 
 const auth = (req, res, next) => {
   const { authorization } = req.headers;
@@ -13,7 +16,10 @@ const auth = (req, res, next) => {
   let payload;
 
   try {
-    payload = jwt.verify(token, 'some-secret-key');
+    payload = jwt.verify(
+      token,
+      NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
+    );
   } catch (err) {
     next(new UnauthorizedError('Необходима авторизация'));
     return;
